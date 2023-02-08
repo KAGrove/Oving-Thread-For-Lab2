5140f9330188c539acd10407888be71f4b106a07
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-08 10:59:06 +0100

    Initial commit

---
+++ venv/thread.py
@@ -0,0 +1,16 @@
+import threading
+import time
+def tråd1(tall, tall2):
+    while True:
+        print(tall, tall2)
+        time.sleep(2)
+
+def tråd2():
+    while True:
+        print("Tråd 2 kjører")
+        time.sleep(2)
+
+t1 = threading.Thread(target=tråd1, args=(1,19))
+t2 = threading.Thread(target=tråd2)
+t1.start()
+t2.start()
